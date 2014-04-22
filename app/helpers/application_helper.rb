7d705852620fc7d1f42acfd256e57096d568f8b2
blueplanet <blueplanet42@gmail.com>
2014-04-22 23:48:04 +0900

    Add devise.ja.yml

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def alert_helper(alert_class='default', content)
+    content_tag 'div', class: "alert alert-#{alert_class} alert-dismissable" do
+      concat content_tag 'button', raw('&times;'), class: "close", :'aria-hidden' => 'true', data: {dismiss: 'alert'}
+      concat content
+    end
+  end
 end
