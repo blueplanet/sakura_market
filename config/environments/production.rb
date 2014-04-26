293228beef0790eeea234423115c1ae2b3d30e4d
blueplanet <blueplanet42@gmail.com>
2014-04-26 16:25:35 +0900

    デフォルトURLオプションを追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,9 +61,12 @@ Rails.application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
 
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'sakura-market.herokuapp.com' }
+
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :domain         => 'heroku.com',
