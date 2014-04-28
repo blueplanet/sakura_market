73db216a34ffff2d5c77f2ad1de25bc11346695f
blueplanet <blueplanet42@gmail.com>
2014-04-28 22:58:36 +0900

    guard-rspec追加

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,6 +43,8 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
 
+  config.include FactoryGirl::Syntax::Methods
+  
   config.before(:suite) do
     FactoryGirl.reload
   end
