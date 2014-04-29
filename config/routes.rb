72ae671940b9a419f52da45189886bdf32602331
blueplanet <blueplanet42@gmail.com>
2014-04-29 22:09:19 +0900

    削除リンクを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :products, only: :index
   resources :carts, only: :show
-  resources :cart_items, only: :create
+  resources :cart_items, only: [:create, :destroy]
 
   root 'products#index'
 end
