b0f2e576f777e0575f542ca68f637b521b11c752
blueplanet <blueplanet42@gmail.com>
2014-04-30 23:52:17 +0900

    カタログデザイン変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.order(:display_no).page params[:page]
+    @products = Product.order(:display_no).page(params[:page]).per(9)
   end
 end
