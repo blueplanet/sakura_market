6f05db3f83b7c77e7034e64f9f46dd4b78979e37
blueplanet <blueplanet42@gmail.com>
2014-04-30 21:37:42 +0900

    カートの合計金額を追加

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,8 @@
 class Cart < ActiveRecord::Base
   belongs_to :user
   has_many :cart_items
+
+  def total_amount
+    cart_items.inject(0) { |sum, item| sum + item.product.price * item.quantity }
+  end
 end
