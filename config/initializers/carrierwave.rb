69e4a55b53269fbeacd6589ccc721f236988d3dc
blueplanet <blueplanet42@gmail.com>
2014-05-08 22:25:33 +0900

    Add image uploader

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,5 @@
 CarrierWave.configure do |config|
-  if Rails.env.test?
-    config.storage :file
-  else
+  if Rails.env.production?
     config.storage = :fog
 
     config.fog_credentials = {
@@ -13,5 +11,7 @@ CarrierWave.configure do |config|
 
     config.fog_directory = ENV['S3_BUCKET_NAME']
     config.fog_public = false
+  else
+    config.storage :file
   end
 end
