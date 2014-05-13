cb780bc10890983d5e52ded1e16279091aa53b77
blueplanet <blueplanet42@gmail.com>
2014-05-13 22:09:38 +0900

    カートアイテムの数を修正出来るように

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show]
   resources :carts, only: :show
-  resources :cart_items, only: [:create, :destroy]
+  resources :cart_items, only: [:create, :update, :destroy]
 
   root 'products#index'
 end
