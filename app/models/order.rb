3f78e9a3adcae611b08d34ef80a3961762b7261a
blueplanet <blueplanet42@gmail.com>
2014-05-22 23:27:29 +0900

    サーバ側制限を追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,30 @@
 class Order < ActiveRecord::Base
+  attr_reader :min_day, :max_day
+
+  validates :name, presence: true
+  validates :delivery_day, presence: true
+  validate :delivery_day_limit
+
+  def initialize
+    super
+
+    @min_day = business_days_after(3)
+    @max_day = business_days_after(14)
+  end
+
+  private
+    def business_days_after num
+      date = Date.today
+
+      while num > 0
+        date += 1
+        num -= 1 if 1 <= date.wday and date.wday <= 5
+      end
+
+      date
+    end
+
+    def delivery_day_limit
+      errors.add(:delivery_day, 'over range of days') if delivery_day < min_day or delivery_day > max_day
+    end
 end
--- spec/helpers/orders_helper_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe OrdersHelper do
-  let(:today) { Date.new 2014, 5, 22 }
-
-  it { expect(helper.business_days_after(3)).to eq 5 }
-  it { expect(helper.business_days_after(14)).to eq 20 }
-end
