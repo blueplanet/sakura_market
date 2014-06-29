889b9f8a5caebd0b2e1226d5e1035c7eeef1a0f6
blueplanet <blueplanet42@gmail.com>
2014-06-30 06:14:50 +0900

    消費税カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140629205347) do
+ActiveRecord::Schema.define(version: 20140629205731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20140629205347) do
     t.integer  "fee"
     t.integer  "user_id"
     t.integer  "total"
+    t.integer  "tax"
   end
 
   add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
