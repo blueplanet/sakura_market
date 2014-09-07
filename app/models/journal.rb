b9e3965bdb5d770c4a90e1cd7e9c85eb11796269
blueplanet <blueplanet42@gmail.com>
2014-09-07 17:15:30 +0900

    Journalモデル追加

---
+++ app/models/journal.rb
@@ -0,0 +1,6 @@
+class Journal < ActiveRecord::Base
+  belongs_to :user
+
+  validates :title, presence: true, length: { in: 4..50 }
+  validates :body, presence: true, length: { in: 4..1000 }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140717200824) do
+ActiveRecord::Schema.define(version: 20140907081117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,16 @@ ActiveRecord::Schema.define(version: 20140717200824) do
   end
 
   add_index "default_addresses", ["user_id"], name: "index_default_addresses_on_user_id", using: :btree
+
+  create_table "journals", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "journals", ["user_id"], name: "index_journals_on_user_id", using: :btree
 
   create_table "orders", force: true do |t|
     t.string   "name"
