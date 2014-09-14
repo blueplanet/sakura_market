472ef8cac856f5e08b176d4a7547376a82f96167
blueplanet <blueplanet42@gmail.com>
2014-09-14 20:57:39 +0900

    Add includes user for comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :journal
 
-  default_scope { order(created_at: :desc) }
+  default_scope { order(created_at: :desc).includes(:user) }
 end
