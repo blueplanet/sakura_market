0054903b81a0cfdd0f1cbe98be71263aae5e7183
blueplanet <erguolinge@gmail.com>
2015-05-03 12:23:17 +0900

    Fix spec error

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe ProfilesController, type: :controller do
 
   describe "GET #show" do
+    let(:profile) { create :profile }
     it "returns http success" do
-      get :show
+      get :show, id: profile.id
       expect(response).to have_http_status(:success)
     end
   end
-
 end
---
+++ spec/factories/profiles.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :profile do
+    user
+    sequence(:nick_name) { |n| "nick_name #{n}" }
+
+    trait :with_avatar do
+      avatar { Rack::Test::UploadedFile.new(File.open(Rails.root.join("spec/fixtures/images/cat.jpg"))) }
+    end
+  end
+end
