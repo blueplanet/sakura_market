a6b54903c513f3bedce5ed9b0ee789249ed598b5
blueplanet <erguolinge@gmail.com>
2015-05-03 11:56:40 +0900

    Add profiles#edit

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,26 @@
 class ProfilesController < ApplicationController
+  before_action :set_profile, only: [:edit, :update]
+
   def show
     @profile = Profile.find params[:id]
+  end
+
+  def update
+    if @profile.update profile_params
+      redirect_to :root, notice: t('.success')
+    else
+      flash.now[:alert] = t('.failed')
+      render :edit
+    end
+  end
+
+  private
+
+  def set_profile
+    @profile = current_user.profile
+  end
+
+  def profile_params
+    params.require(:profile).permit(:nick_name, :avatar, :avatar_cache)
   end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -14,4 +14,9 @@ class Profile < ActiveRecord::Base
   belongs_to :user
 
   mount_uploader :avatar, AvatarUploader
+
+  with_options on: :update do
+    validates :nick_name, presence: true
+    validates :avatar, presence: true
+  end
 end
