932318e40a6649419c1a05fd11477c34303eeb29
blueplanet <erguolinge@gmail.com>
2015-05-05 07:43:49 +0900

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-
-  devise_for :users
-
   resources :products, only: [:index, :show]
+
   resource :cart, only: :show
   resources :orders, only: [:new, :create, :index, :show]
   resources :cart_items, only: [:create, :update, :destroy]
+  resource :default_address, only: [:edit, :update]
+
   resources :journals do
     resources :comments, only: [:create, :destroy]
     resources :goods, only: :create
@@ -15,7 +13,11 @@ Rails.application.routes.draw do
 
   resources :profiles, only: :show
   resource :profile, only: [:edit, :update]
-  resource :default_address, only: [:edit, :update]
 
   root 'journals#index'
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
+  devise_for :users
 end
